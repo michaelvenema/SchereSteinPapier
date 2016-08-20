d89fa4173c0ef6320d155f7c7d1f213b9598db88
michi <michi@BenjaminBritten>
2016-08-20 15:49:22 +0200

    implemetiere Methode getSpieler

--- src/main/java/de/mvenema/schere_stein_papier/Spiel.java
+++ src/main/java/de/mvenema/schere_stein_papier/Spiel.java
@@ -3,13 +3,24 @@
  */
 package de.mvenema.schere_stein_papier;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 /**
  * @author michi
  *
  */
-public class Spiel {
+public class Spiel {	
+
+	protected static final int DEFAULT_ANZAHL_SPIELZUEGE = 10;
+	private final ArrayList<Spieler> spielerList = new ArrayList<Spieler>();
 	
-	public Spiel() {
-		
+	public Spiel(final Spieler p_spieler1, final Spieler p_spieler2) {
+		this.spielerList.add(p_spieler1);
+		this.spielerList.add(p_spieler2);
+	}
+
+	public ArrayList<Spieler> getSpieler() {
+		return this.spielerList;
 	}
 }
