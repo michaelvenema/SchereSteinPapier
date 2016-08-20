996a511e38d408b9a3b1cfa264f090311f0e4551
michi <michi@BenjaminBritten>
2016-08-20 15:48:47 +0200

    implementiere Testmethode testeInitSpielErwarte2DefinierteSpieler

--- src/test/java/de/mvenema/schere_stein_papier/TesteSpiel.java
+++ src/test/java/de/mvenema/schere_stein_papier/TesteSpiel.java
@@ -11,13 +11,35 @@ import org.junit.Test;
  *
  */
 public class TesteSpiel {
-	
+
 	@Test
 	public void testeInitSpielErwarteInstanz() {
 		SoftAssertions softly = new SoftAssertions();
-		Spiel spiel = new Spiel();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1);
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2);
+		Spiel spiel = new Spiel(spieler1, spieler2);
 		softly.assertThat(spiel).isNotNull();
 		softly.assertThat(spiel).isInstanceOf(Spiel.class);
-		softly.assertAll();		
+		softly.assertAll();
+	}
+
+	@Test
+	public void testeInitSpielErwarteAnzahlSpieler2() {
+		SoftAssertions softly = new SoftAssertions();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1);
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2);
+		Spiel spiel = new Spiel(spieler1, spieler2);
+		softly.assertThat(spiel.getSpieler()).size().isEqualTo(2);
+		softly.assertAll();
+	}
+
+	@Test
+	public void testeInitSpielErwarte2DefinierteSpieler() {
+		SoftAssertions softly = new SoftAssertions();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1);
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2);
+		Spiel spiel = new Spiel(spieler1, spieler2);
+		softly.assertThat(spiel.getSpieler()).containsExactly(spieler1, spieler2);
+		softly.assertAll();
 	}
 }
