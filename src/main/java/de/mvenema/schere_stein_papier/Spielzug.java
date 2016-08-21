3635bafd07f439b3e83cd534795075a600188a6c
michi <michi@BenjaminBritten>
2016-08-21 12:53:48 +0200

    implementiere und löse
    TestePielzug.testeHoleWuerfeErwarteSymbole

--- src/main/java/de/mvenema/schere_stein_papier/Spielzug.java
+++ src/main/java/de/mvenema/schere_stein_papier/Spielzug.java
@@ -7,21 +7,25 @@ import java.util.HashMap;
 public class Spielzug {
 
 	public enum Symbol {
-
+		SCHERE,
+		STEIN,
+		PAPIER;
 	}
 	
+	private final ArrayList<Spieler> spielerList;
 	private HashMap<Spieler, Spielzug.Symbol> spielzugMap = new HashMap<Spieler, Spielzug.Symbol>();
 
 	public Spielzug(final ArrayList<Spieler> p_spielerList) {
 		if (p_spielerList != null && p_spielerList.size() == 2 && p_spielerList.get(0) != null && p_spielerList.get(1) != null) {
-			this.spielzugMap.put(p_spielerList.get(0), null);
-			this.spielzugMap.put(p_spielerList.get(1), null);
+			this.spielerList = p_spielerList;
 		} else {
 			throw new IllegalArgumentException("Die Spielerliste muss genau 2 Spieler enthalten");
 		}
 	}
 
 	public HashMap<Spieler, Spielzug.Symbol> getWuerfe() {
+		this.spielzugMap.put(this.spielerList.get(0), Symbol.PAPIER);
+		this.spielzugMap.put(spielerList.get(1), Symbol.PAPIER);
 		return this.spielzugMap;
 	}
 
--- src/test/java/de/mvenema/schere_stein_papier/TesteSpielzug.java
+++ src/test/java/de/mvenema/schere_stein_papier/TesteSpielzug.java
@@ -4,6 +4,7 @@
 package de.mvenema.schere_stein_papier;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.assertj.core.api.SoftAssertions;
 import org.hamcrest.core.IsInstanceOf;
@@ -53,6 +54,48 @@ public class TesteSpielzug {
 		spielerList.add(new Spieler(TesteSpieler.NAME_SPIELER2));
 		Spielzug spielzug = new Spielzug(spielerList);
 		softly.assertThat(spielzug.getWuerfe()).size().isEqualTo(2);
+		softly.assertAll();
+	}
+	
+	@Test
+	public void testeHoleWuerfeErwarteHashMap() {
+		SoftAssertions softly = new SoftAssertions();
+		ArrayList<Spieler> spielerList = new ArrayList<>();
+		spielerList.add(new Spieler(TesteSpieler.NAME_SPIELER1));
+		spielerList.add(new Spieler(TesteSpieler.NAME_SPIELER2));
+		Spielzug spielzug = new Spielzug(spielerList);
+		softly.assertThat(spielzug.getWuerfe()).isInstanceOf(HashMap.class);
+		softly.assertAll();
+	}
+	
+	@Test
+	public void testeHoleWuerfeErwarteSpieler() {
+		SoftAssertions softly = new SoftAssertions();
+		ArrayList<Spieler> spielerList = new ArrayList<>();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1);
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2);
+		spielerList.add(spieler1);
+		spielerList.add(spieler2);
+		Spielzug spielzug = new Spielzug(spielerList);
+		HashMap<Spieler, Spielzug.Symbol> wuerfe = spielzug.getWuerfe();
+		softly.assertThat(wuerfe.containsKey(spieler1));
+		softly.assertThat(wuerfe.containsKey(spieler2));
+
+		softly.assertAll();
+	}
+	
+	@Test
+	public void testeHoleWuerfeErwarteSymbole() {
+		SoftAssertions softly = new SoftAssertions();
+		ArrayList<Spieler> spielerList = new ArrayList<>();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1);
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2);
+		spielerList.add(spieler1);
+		spielerList.add(spieler2);
+		Spielzug spielzug = new Spielzug(spielerList);
+		HashMap<Spieler, Spielzug.Symbol> wuerfe = spielzug.getWuerfe();
+		softly.assertThat(wuerfe.get(spieler1)).isInstanceOf(Spielzug.Symbol.class);
+		softly.assertThat(wuerfe.get(spieler2)).isInstanceOf(Spielzug.Symbol.class);
 		softly.assertAll();
 	}
 }
