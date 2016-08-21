1af131b84013bf9ac8ce418586f0d5c12287def8
michi <michi@BenjaminBritten>
2016-08-21 16:41:28 +0200

    implementiere Tests für Spielergebnis

--- src/main/java/de/mvenema/schere_stein_papier/Spiel.java
+++ src/main/java/de/mvenema/schere_stein_papier/Spiel.java
@@ -10,13 +10,48 @@ import java.util.ArrayList;
  * @author michi
  *
  */
-public class Spiel {	
+public class Spiel {
+
+	public class Spielergebnis {
+
+		public Spielergebnis() {
+		}
+
+		public ArrayList<Spielzug> getGespielteZuege() {
+			return Spiel.this.spielZuege;
+		}
+
+		public ArrayList<Spieler> getSpieler() {
+			return Spiel.this.spielerList;
+		}
+
+		public int getSiege(final Spieler p_spieler) {
+			int result = 0;
+			for (Spielzug spielzug : Spiel.this.spielZuege) {
+				if (spielzug.werIstSieger().size() > 0 && spielzug.werIstSieger().get(0).equals(p_spieler)) {
+					result += 1;
+				}
+			}
+			return result;
+		}
+
+		public int getUnentschieden() {
+			int result = 0;
+			for (Spielzug spielzug : Spiel.this.spielZuege) {
+				if (spielzug.werIstSieger().size() == 0) {
+					result += 1;
+				}
+			}
+			return result;
+		}
+	}
 
 	protected static final int DEFAULT_ANZAHL_SPIELZUEGE = 10;
 	private final ArrayList<Spieler> spielerList = new ArrayList<Spieler>();
 	private int anzahlSpielZuege = 0;
 	private ArrayList<Spielzug> spielZuege = new ArrayList<Spielzug>();
-	
+	private Spielergebnis spielergebnis = null;
+
 	public Spiel(final Spieler p_spieler1, final Spieler p_spieler2) {
 		this.spielerList.add(p_spieler1);
 		this.spielerList.add(p_spieler2);
@@ -31,17 +66,27 @@ public class Spiel {
 	}
 
 	public void spieleSpiel() {
-		this.anzahlSpielZuege = DEFAULT_ANZAHL_SPIELZUEGE;
+		this.spieleSpiel(DEFAULT_ANZAHL_SPIELZUEGE);
 	}
 
 	public void spieleSpiel(final int p_anzahlSpielzuege) {
-		this.anzahlSpielZuege = p_anzahlSpielzuege;		
-	}
-
-	public ArrayList<Spielzug> getGespielteZuege() {
+		this.anzahlSpielZuege = p_anzahlSpielzuege;
 		for (int i = 1; i <= this.anzahlSpielZuege; i++) {
 			this.spielZuege.add(new Spielzug(this.spielerList));
 		}
+		this.spielergebnis = new Spielergebnis();
+	}
+
+	/**
+	 * @deprecated use Spielergebnis.getGespielteZuge instead
+	 * @return
+	 */
+	@Deprecated
+	public ArrayList<Spielzug> getGespielteZuege() {
 		return this.spielZuege;
+	}
+
+	public Spielergebnis getSpielergebnis() {
+		return this.spielergebnis;
 	}
 }
--- src/test/java/de/mvenema/schere_stein_papier/TesteSpiel.java
+++ src/test/java/de/mvenema/schere_stein_papier/TesteSpiel.java
@@ -6,6 +6,8 @@ package de.mvenema.schere_stein_papier;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Test;
 
+import de.mvenema.schere_stein_papier.Spiel.Spielergebnis;
+
 /**
  * @author michi
  *
@@ -87,4 +89,113 @@ public class TesteSpiel {
 		softly.assertThat(spiel.getGespielteZuege()).size().isEqualTo(anzahlSpielzuege);
 		softly.assertAll();
 	}
+	
+	@Test
+	public void initialisiereSpielHoleSpielergebnisErwarteNull() {
+		SoftAssertions softly = new SoftAssertions();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1, new StrategieImmerStein());
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2, new StrategieZufall());
+		Spiel spiel = new Spiel(spieler1, spieler2);
+		softly.assertThat(spiel.getSpielergebnis()).isNull();
+		softly.assertAll();
+	}
+	
+	@Test
+	public void spieleSpielHoleSpielergebnisErwarteInstanz() {
+		int anzahlSpielzuege = 100;
+		SoftAssertions softly = new SoftAssertions();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1, new StrategieImmerStein());
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2, new StrategieZufall());
+		Spiel spiel = new Spiel(spieler1, spieler2);
+		spiel.spieleSpiel(anzahlSpielzuege);
+		softly.assertThat(spiel.getSpielergebnis()).isInstanceOf(Spiel.Spielergebnis.class);
+		softly.assertAll();
+	}
+	
+	@Test
+	public void starteSpielErwarteAnzahlGespielterZuegeAusSpielergebnis() {
+		int anzahlSpielzuege = 100;
+		SoftAssertions softly = new SoftAssertions();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1, new StrategieImmerStein());
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2, new StrategieZufall());
+		Spiel spiel = new Spiel(spieler1, spieler2);
+		spiel.spieleSpiel(anzahlSpielzuege);
+		softly.assertThat(spiel.getSpielergebnis().getGespielteZuege()).size().isEqualTo(anzahlSpielzuege);
+		softly.assertAll();
+	}
+	
+	@Test
+	public void starteSpielErwarteSpielerAusSpielergebnis() {
+		int anzahlSpielzuege = 100;
+		SoftAssertions softly = new SoftAssertions();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1, new StrategieImmerStein());
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2, new StrategieZufall());
+		Spiel spiel = new Spiel(spieler1, spieler2);
+		spiel.spieleSpiel(anzahlSpielzuege);
+		Spielergebnis spielergebnis = spiel.getSpielergebnis();
+		softly.assertThat(spielergebnis.getSpieler()).containsExactly(spieler1, spieler2);
+		softly.assertAll();
+	}
+	
+	@Test
+	public void starteSpielErwarteSiegeSpieler1AusSpielergebnis() {
+		int anzahlSpielzuege = 100;
+		SoftAssertions softly = new SoftAssertions();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1, new StrategieImmerStein());
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2, new StrategieZufall());
+		Spiel spiel = new Spiel(spieler1, spieler2);
+		spiel.spieleSpiel(anzahlSpielzuege);
+		Spielergebnis spielergebnis = spiel.getSpielergebnis();
+		softly.assertThat(spielergebnis.getSiege(spieler1)).isBetween(20, 50);
+		softly.assertAll();
+	}
+	
+	@Test
+	public void starteSpielErwarteSiegeSpieler2AusSpielergebnis() {
+		int anzahlSpielzuege = 100;
+		SoftAssertions softly = new SoftAssertions();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1, new StrategieImmerStein());
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2, new StrategieZufall());
+		Spiel spiel = new Spiel(spieler1, spieler2);
+		spiel.spieleSpiel(anzahlSpielzuege);
+		Spielergebnis spielergebnis = spiel.getSpielergebnis();
+		softly.assertThat(spielergebnis.getSiege(spieler2)).isBetween(20, 50);
+		softly.assertAll();
+	}
+	
+	@Test
+	public void starteSpielErwarteUnentschiedenAusSpielergebnis() {
+		int anzahlSpielzuege = 100;
+		SoftAssertions softly = new SoftAssertions();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1, new StrategieImmerStein());
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2, new StrategieZufall());
+		Spiel spiel = new Spiel(spieler1, spieler2);
+		spiel.spieleSpiel(anzahlSpielzuege);
+		Spielergebnis spielergebnis = spiel.getSpielergebnis();
+		softly.assertThat(spielergebnis.getUnentschieden()).isBetween(20, 50);
+		softly.assertAll();
+	}
+	
+	@Test
+	public void starteSpielErwarteSummeWieAnzahlZuege() {
+		int anzahlSpielzuege = 100;
+		SoftAssertions softly = new SoftAssertions();
+		Spieler spieler1 = new Spieler(TesteSpieler.NAME_SPIELER1, new StrategieImmerStein());
+		Spieler spieler2 = new Spieler(TesteSpieler.NAME_SPIELER2, new StrategieZufall());
+		Spiel spiel = new Spiel(spieler1, spieler2);
+		spiel.spieleSpiel(anzahlSpielzuege);
+		Spielergebnis spielergebnis = spiel.getSpielergebnis();
+		int siegeSpieler1 = spielergebnis.getSiege(spieler1);
+		int siegeSpieler2 = spielergebnis.getSiege(spieler2);
+		int unentschieden = spielergebnis.getUnentschieden();
+		int summe = siegeSpieler1 + siegeSpieler2 + unentschieden;
+		softly.assertThat(summe).isEqualTo(anzahlSpielzuege);
+		System.out.println(siegeSpieler1);
+		System.out.println(siegeSpieler2);
+		System.out.println(unentschieden);
+		System.out.println(summe);
+		softly.assertAll();
+	}
+	
+	
 }
